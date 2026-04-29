58d6a27b14d728e12af722412e80541320d82b19
bonnie12-w <bonfacekuggie@gmail.com>
2026-04-29 16:10:32 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,9 +39,9 @@ const Navbar = () => {
           <span className="text-2xl">🔥</span>
           <div>
             <h1 className="font-display text-lg sm:text-xl font-bold gold-gradient-text leading-tight">
-              bigsmokekaren
+              Papparoti Kenya
             </h1>
-            <p className="text-[10px] tracking-[0.3em] uppercase text-muted-foreground">BS</p>
+            <p className="text-[10px] tracking-[0.3em] uppercase text-muted-foreground">PK</p>
           </div>
         </Link>
 
